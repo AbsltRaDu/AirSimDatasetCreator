f0ccafaa8c4ae97a25fc7a87b803ef47e776a1d9
AbsltRaDu <jareniyponchik.da@gmail.com>
2026-05-05 00:28:49 +0300

    Протестировал сбор изображений

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import airsim
+import time
+import os 
+import cv2
+import numpy as np
 
 client = airsim.MultirotorClient(ip="172.31.240.1", port=41451)
 client.confirmConnection()
@@ -12,16 +16,50 @@ print('Моторы включены')
 client.takeoffAsync().join() # Взлет
 print('Взлет')
 
-print('Начато движение к цели')
+os.makedirs('dataset_0/cam0/data', exist_ok=True)
+os.makedirs('dataset_0/cam1/data', exist_ok=True)
 
-client.moveToPositionAsync(
-    x=10,
-    y=0,
-    z=-5,
-    velocity=3
-).join()
+for i in range(100):
+    
+    timestamp = int(time.time() * 1e9)  # наносекунды как в EuRoC
+    
+    # Получаем изображения
+    responses = client.simGetImages([
+        airsim.ImageRequest('0', airsim.ImageType.Scene, False, False),
+        airsim.ImageRequest('1', airsim.ImageType.Scene, False, False)
+    ])
+    
+    # Обрабатываем изображения
+    img0 = np.frombuffer(responses[0].image_data_uint8, dtype=np.uint8)
+    img0 = img0.reshape(responses[0].height, responses[0].width, 3)
+    
+    img1 = np.frombuffer(responses[1].image_data_uint8, dtype=np.uint8)
+    img1 = img1.reshape(responses[1].height, responses[1].width, 3)
 
-print('Движение завершено')
+    # сохранение
+    cv2.imwrite(f"dataset_0/cam0/data/{timestamp}.png", img0)
+    cv2.imwrite(f"dataset_0/cam1/data/{timestamp}.png", img1)
+
+    # TODO частоты не как в EuRoC
+    imu = client.getImuData()
+    # TODO координаты не как в EuRoC
+    pose = client.simGetVehiclePose()
+    
+    print('IMU:', imu)
+    print('Pose:', pose)
+    
+    time.sleep(0.1)
+    
+# print('Начато движение к цели')
+
+# client.moveToPositionAsync(
+#     x=10,
+#     y=0,
+#     z=-5,
+#     velocity=3
+# ).join()
+
+# print('Движение завершено')
 
 
 print('Выполняю посадку')
